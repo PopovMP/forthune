3023dddb51886b52e6332f83706ca02b5ef807ea
Miroslav Popov <info@forexsb.com>
2022-11-19 08:56:59 +0000

    Refactored DO.

--- js/index.js
+++ js/index.js
@@ -544,46 +544,13 @@ class Executor {
                         break;
                     }
                     if (token.word === 'DO' || token.word === '?DO') {
-                        const isQuestionDup = token.word === '?DO';
-                        let loopIndex = i + 1;
-                        let doDepth = 1;
-                        while (true) {
-                            loopIndex += 1;
-                            if (loopIndex === tokens.length)
-                                return { status: 1 /* Status.Fail */, value: 'LOOP E: Not found' };
-                            const loopWord = tokens[loopIndex].value.toUpperCase();
-                            if (loopWord === 'DO')
-                                doDepth += 1;
-                            if (loopWord === 'LOOP' || loopWord === '+LOOP')
-                                doDepth -= 1;
-                            if (doDepth === 0)
-                                break;
-                        }
-                        const isPlusLoop = tokens[loopIndex].value.toUpperCase() === '+LOOP';
-                        let counter = env.dStack.pop();
-                        const limit = env.dStack.pop();
-                        const upwards = limit > counter;
-                        if (isQuestionDup && counter === limit) {
-                            i = loopIndex;
-                            env.isLeave = false;
-                            continue;
-                        }
-                        if (!isPlusLoop && !upwards)
-                            return { status: 1 /* Status.Fail */, value: 'LOOP E: Wrong range' };
-                        while (upwards ? counter < limit : counter >= limit) {
-                            env.rStack.push(counter);
-                            const res = Executor.run(tokens.slice(i + 1, loopIndex), env);
-                            env.rStack.pop();
-                            if (env.isLeave)
-                                break;
-                            outText += res.value;
-                            if (res.status === 1 /* Status.Fail */)
-                                return { status: 1 /* Status.Fail */, value: outText };
-                            counter += isPlusLoop ? env.dStack.pop() : 1;
-                        }
-                        i = loopIndex;
-                        env.isLeave = false;
-                        continue;
+                        const res = Executor.runDO(tokens, i, env);
+                        outText += res.value;
+                        if (res.status === 1 /* Status.Fail */)
+                            return { status: 1 /* Status.Fail */, value: outText };
+                        if (typeof res.newIndex === 'number')
+                            i = res.newIndex;
+                        break;
                     }
                     if (Dictionary.colonDef.hasOwnProperty(token.word)) {
                         const res = Executor.run(Dictionary.colonDef[token.word].tokens, env);
@@ -660,6 +627,50 @@ class Executor {
         }
         // Continuation
         return { status: 0 /* Status.Ok */, value: '', newIndex: thenIndex };
+    }
+    static runDO(tokens, index, env) {
+        // Find LOOP index
+        let loopIndex = index + 1;
+        let doDepth = 1;
+        while (true) {
+            loopIndex += 1;
+            if (loopIndex === tokens.length)
+                return { status: 1 /* Status.Fail */, value: 'LOOP Not found' };
+            const loopWord = tokens[loopIndex].value.toUpperCase();
+            if (loopWord === 'DO')
+                doDepth += 1;
+            if (loopWord === 'LOOP' || loopWord === '+LOOP')
+                doDepth -= 1;
+            if (doDepth === 0)
+                break;
+        }
+        let counter = env.dStack.pop();
+        const limit = env.dStack.pop();
+        const upwards = limit > counter;
+        if (tokens[index].word === '?DO' && counter === limit) {
+            // No entry in the loop
+            env.isLeave = false;
+            return { status: 0 /* Status.Ok */, value: '', newIndex: loopIndex };
+        }
+        const isPlusLoop = tokens[loopIndex].word === '+LOOP';
+        if (!isPlusLoop && !upwards)
+            return { status: 1 /* Status.Fail */, value: 'LOOP Wrong range' };
+        let outText = '';
+        while (upwards ? counter < limit : counter >= limit) {
+            env.rStack.push(counter);
+            const doBody = tokens.slice(index + 1, loopIndex);
+            const res = Executor.run(doBody, env);
+            env.rStack.pop();
+            if (env.isLeave)
+                break;
+            outText += res.value;
+            if (res.status === 1 /* Status.Fail */)
+                return { status: 1 /* Status.Fail */, value: outText };
+            counter += isPlusLoop ? env.dStack.pop() : 1;
+        }
+        // Continuation
+        env.isLeave = false;
+        return { status: 0 /* Status.Ok */, value: outText, newIndex: loopIndex };
     }
 }
 class Forth {
--- src/executor.ts
+++ src/executor.ts
@@ -54,58 +54,16 @@ class Executor
 					}
 
 					if (token.word === 'DO' || token.word === '?DO') {
-						const isQuestionDup = token.word === '?DO'
-						let loopIndex = i + 1
-						let doDepth   = 1
-
-						while (true) {
-							loopIndex += 1
-							if (loopIndex === tokens.length)
-								return {status: Status.Fail, value: 'LOOP E: Not found'}
-							const loopWord = tokens[loopIndex].value.toUpperCase()
-							if (loopWord === 'DO')
-								doDepth += 1
-							if (loopWord === 'LOOP' || loopWord === '+LOOP')
-								doDepth -= 1
-							if (doDepth === 0)
-								break
-						}
-
-						const isPlusLoop = tokens[loopIndex].value.toUpperCase() === '+LOOP'
-						let   counter    = env.dStack.pop()
-						const limit      = env.dStack.pop()
-						const upwards    = limit > counter
-
-						if (isQuestionDup && counter === limit) {
-							i = loopIndex
-							env.isLeave = false
-							continue
-						}
-
-						if (!isPlusLoop && !upwards)
-							return {status: Status.Fail, value: 'LOOP E: Wrong range'}
-
-						while (upwards ? counter < limit : counter >= limit) {
-							env.rStack.push(counter)
-							const res = Executor.run(tokens.slice(i+1, loopIndex), env)
-							env.rStack.pop()
-
-							if (env.isLeave)
-								break
-
-							outText += res.value
-							if (res.status === Status.Fail)
-								return {status: Status.Fail, value: outText}
-
-							counter += isPlusLoop ? env.dStack.pop() : 1
-						}
-
-						i = loopIndex
-						env.isLeave = false
-						continue
+						const res = Executor.runDO(tokens, i, env)
+						outText += res.value
+						if (res.status === Status.Fail)
+							return {status: Status.Fail, value: outText}
+						if (typeof res.newIndex === 'number')
+							i = res.newIndex
+						break
 					}
 
-					if (Dictionary.colonDef.hasOwnProperty(token.word)) {
+					if ( Dictionary.colonDef.hasOwnProperty(token.word) ) {
 						const res = Executor.run(Dictionary.colonDef[token.word].tokens, env)
 						outText += res.value
 						if (res.status === Status.Fail)
@@ -113,17 +71,17 @@ class Executor
 						break
 					}
 
-					if (env.value.hasOwnProperty(token.word)) {
+					if ( env.value.hasOwnProperty(token.word) ) {
 						env.dStack.push(env.value[token.word])
 						continue
 					}
 
-					if (env.constant.hasOwnProperty(token.word)) {
+					if ( env.constant.hasOwnProperty(token.word) ) {
 						env.dStack.push(env.constant[token.word])
 						continue
 					}
 
-					if (Dictionary.words.hasOwnProperty(token.word) ) {
+					if ( Dictionary.words.hasOwnProperty(token.word) ) {
 						const res = Dictionary.words[token.word](env)
 						outText += res.value
 						if (res.status === Status.Fail)
@@ -193,5 +151,60 @@ class Executor
 
 		// Continuation
 		return {status: Status.Ok, value: '', newIndex: thenIndex}
+	}
+
+	public static runDO(tokens: Token[], index :number, env: Environment): ExecResult
+	{
+		// Find LOOP index
+		let loopIndex = index + 1
+		let doDepth   = 1
+		while (true) {
+			loopIndex += 1
+			if (loopIndex === tokens.length)
+				return {status: Status.Fail, value: 'LOOP Not found'}
+			const loopWord = tokens[loopIndex].value.toUpperCase()
+			if (loopWord === 'DO')
+				doDepth += 1
+			if (loopWord === 'LOOP' || loopWord === '+LOOP')
+				doDepth -= 1
+			if (doDepth === 0)
+				break
+		}
+
+		let   counter = env.dStack.pop()
+		const limit   = env.dStack.pop()
+		const upwards = limit > counter
+
+		if (tokens[index].word === '?DO' && counter === limit) {
+			// No entry in the loop
+			env.isLeave = false
+			return {status: Status.Ok, value: '', newIndex: loopIndex}
+		}
+
+		const isPlusLoop = tokens[loopIndex].word === '+LOOP'
+
+		if (!isPlusLoop && !upwards)
+			return {status: Status.Fail, value: 'LOOP Wrong range'}
+
+		let outText = ''
+		while (upwards ? counter < limit : counter >= limit) {
+			env.rStack.push(counter)
+			const doBody = tokens.slice(index+1, loopIndex)
+			const res = Executor.run(doBody , env)
+			env.rStack.pop()
+
+			if (env.isLeave)
+				break
+
+			outText += res.value
+			if (res.status === Status.Fail)
+				return {status: Status.Fail, value: outText}
+
+			counter += isPlusLoop ? env.dStack.pop() : 1
+		}
+
+		// Continuation
+		env.isLeave = false
+		return {status: Status.Ok, value: outText, newIndex: loopIndex}
 	}
 }
