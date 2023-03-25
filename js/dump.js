0250623e6f880ddfe032e08bf25ae286f65303af
Miroslav Popov <info@forexsb.com>
2023-03-25 15:05:12 +0000

    Added f64text dump

--- js/dump.js
+++ js/dump.js
@@ -1,16 +1,16 @@
 'use strict'
 
 /**
+ * Dumps the VM memory
  *
  * @param {HTMLElement} memDump
- *
  * @param {(addr: number) => number} cFetch
  * @param {number} MEM_SIZE
  */
 function dump(memDump, cFetch, MEM_SIZE)
 {
 	const ADDR_STEP = 16
-	const lines = [		' Dec  Hex     0 1  2 3  4 5  6 7  8 9  A B  C D  E F ']
+	const lines = [		' Dec  Hex    0 1  2 3  4 5  6 7  8 9  A B  C D  E F ']
 	let previousLineText = ''
 	let isStar = false
 	for (let lineAddr = 0; lineAddr < MEM_SIZE; lineAddr += ADDR_STEP) {
@@ -24,7 +24,7 @@ function dump(memDump, cFetch, MEM_SIZE)
 			if (!isStar) {
 				lines.push(
 					lineAddr.toString(10).padStart(5, '0') + ' ' +
-					lineAddr.toString(16).padStart(4, '0').toUpperCase() + ' : * ')
+					lineAddr.toString(16).padStart(4, '0').toUpperCase() + ': * ')
 				isStar = true
 			}
 			continue
@@ -33,19 +33,48 @@ function dump(memDump, cFetch, MEM_SIZE)
 		isStar = false
 		let lineText  = ''
 		let asciiText = ''
+
 		for (let i = 0; i < ADDR_STEP; i += 2) {
 			const ch1 = /** @type {number} */ lineBytes[i]
 			const ch2 = /** @type {number} */ lineBytes[i+1]
 
-			lineText += ch1.toString(16).padStart(2, '0').toUpperCase() +
-						ch2.toString(16).padStart(2, '0').toUpperCase() + ' '
-			asciiText += (31 < ch1 && ch1 !== 126 && ch1 < 256 ? String.fromCharCode(ch1) : '.') +
-						 (31 < ch2 && ch2 !== 126 && ch2 < 256 ? String.fromCharCode(ch2) : '.')
+			lineText  += ch1.toString(16).padStart(2, '0').toUpperCase() +
+						 ch2.toString(16).padStart(2, '0').toUpperCase() + ' '
+			asciiText += (31 < ch1 && ch1 < 126 ? String.fromCharCode(ch1) : '.') +
+						 (31 < ch2 && ch2 < 126 ? String.fromCharCode(ch2) : '.')
 		}
+
+		const ab = new ArrayBuffer(16)
+		new Uint8Array(ab).set(lineBytes)
+		const f64Bytes = new Float64Array(ab)
+		const f64Text  = f64ToText(f64Bytes[0]) + ' ' + f64ToText(f64Bytes[1])
 		lines.push(
 			lineAddr.toString(10).padStart(5, '0') + ' ' +
-			lineAddr.toString(16).padStart(4, '0').toUpperCase() + ' : ' + lineText + '  ' + asciiText)
+			lineAddr.toString(16).padStart(4, '0').toUpperCase() + ': ' + lineText + asciiText + ' ' + f64Text)
 	}
+
+	memDump.innerText = lines.join('\n')
+}
+
+function f64ToText(num)
+{
+	if (num >= 0 && Number.isInteger(num)) {
+		let nn = num < 70000 ? num : num % 100_000
+		return nn.toString().padStart(5, ' ')
+	}
+
+	return '     '
+}
+
+/**
+ * Dumps stacks and registers
+ *
+ * @param {HTMLElement} memDump
+ * @param {(addr: number) => number} cFetch
+ */
+function debug(memDump, cFetch)
+{
+	const lines = ['foo']
 
 	memDump.innerText = lines.join('\n')
 }
